c3cfc6fcaeea92c5f4898f7373a0baf370cc84da
dpembe2 <dpembe2@uw.edu>
2026-02-19 07:35:48 -0800

    Update homework8_Deborah_Pemberton.py

--- homework8_Deborah_Pemberton.py
+++ homework8_Deborah_Pemberton.py
@@ -8,9 +8,10 @@ pizza_orders = [
 finished_pizzas = []
 while pizza_orders:
     current_pizza = pizza_orders.pop(0)
-    print(f"{current_pizza}: Your pizza pie is finished!")
-finished_pizzas.append(current_pizza)
+    print(f"{current_pizza['name']}: Your pizza pie is finished!")
+    finished_pizzas.append(current_pizza)
+
 index = 0
 while index < len(finished_pizzas):
-    print(f"The pizza {finished_pizzas['name']} was made.")
-    index += 1
+    print(f"The pizza {finished_pizzas[index]['name']} was made.")
+    index += 
